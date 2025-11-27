340ecabd841a3300b1ffb131da76dea02a2eb666
GGOORINA\82107 <justina04@hanmail.net>
2025-11-28 08:25:06 +0900

    서버배포, db연결완료

--- Server/build.gradle.kts
+++ Server/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("com.mysql:mysql-connector-j:9.1.0")
+	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+
 
 }
 
--- Server/src/main/kotlin/com/mobApp/roulette/config/SecurityConfig.kt
+++ Server/src/main/kotlin/com/mobApp/roulette/config/SecurityConfig.kt
@@ -5,23 +5,27 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
-                // CSRF 비활성화 (Spring Security 6.1+ 방식)
                 .csrf { it.disable() }
-                // 모든 요청 허용
-                .authorizeHttpRequests { auth ->
-                    auth.anyRequest().permitAll()
+                .authorizeHttpRequests {
+                    it.requestMatchers(
+                            "/swagger-ui/**",
+                            "/v3/api-docs/**",
+                            "/swagger-resources/**",
+                            "/webjars/**"
+                    ).permitAll()
+                    it.anyRequest().permitAll()   // 필요하면 authenticated()로 변경
                 }
-                // 기본 로그인 화면 제거
-                .formLogin { it.disable() }
-                // HTTP Basic 인증 비활성화
                 .httpBasic { it.disable() }
+                .formLogin { it.disable() }
 
         return http.build()
     }
